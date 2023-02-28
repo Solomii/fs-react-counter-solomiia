f8a9ddcf84e463825ee5ace70554d4e585c1b1d4
Solomiia Kushniryk <kushniryksolomiya@gmail.com>
2023-02-28 16:17:50 +0200

    Add change mode and decrement

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,27 +1,36 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      isAdd:true,
-      count:0
+    constructor(props) {
+        super(props);
+        this.state = {
+            isAdd: true,
+            count: 0,
+        };
     }
+    stepCount = () => {
+        const { isAdd, count } = this.state;
+        this.setState({ count: isAdd ? count + 1 : count - 1 });
+    };
 
-  
-  }
+    handlerChangeMode = () => {
+        const { isAdd } = this.state;
+        this.setState({ isAdd: !isAdd });
+    };
     render() {
-      const {step} = this.props;
-        const {count } = this.state;
-      
+        const { step } = this.props;
+        const { count } = this.state;
 
         return (
             <div>
-                Counter : {count };
+                Counter : {count}
+                <br />
                 {/* за станом змінювати кновку  */}
-                <button onClick={() => this.setState({ count:count + 1 })}>+/-</button>
-                 {/* по кліку  змінювати стан  isAdd  */}
-                <button onClick={() => {}}>change mode</button>
+                <button className="btn" onClick={this.stepCount}>
+                    +/-
+                </button>
+                {/* по кліку  змінювати стан  isAdd  */}
+                <button onClick={this.handlerChangeMode}>change mode</button>
             </div>
         );
     }
